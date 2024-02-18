b4ed365769bbf89a91e79500a63eac63b4700e00
Anna Soboleva <asoboleva.avs@gmail.com>
2024-02-19 00:47:16 +0300

    added subtraction_two_numbers(number1, number2) and changed sum_two_number(number1, number2) to sum_two_numbers(number1, number2)

--- funcs.py
+++ funcs.py
@@ -1,2 +1,5 @@
-def sum_two_number(number1, number2):
+def sum_two_numbers(number1, number2):
     return number1 + number2
+
+def subtraction_two_numbers(number1, number2):
+    return number1 - number2
